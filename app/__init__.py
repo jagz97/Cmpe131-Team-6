3e13372861b61ecca3548d035d69c84d8818c016
jagjitsingh <jagjit.saini2019@gmail.com>
2022-04-30 23:21:18 -0700

    functional requirement add item to seller and add pictures for items and display on main page completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import IMAGES, UploadSet, configure_uploads
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -7,10 +9,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
 app.config.from_mapping(
-    SECRET_KEY = 'you-will-never-know',
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test.db'),
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    )
+    SECRET_KEY='you-will-never-know',
+    SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir, 'test.db'),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+)
+# Flask-Uploads configration provided on Documentation Page
+app.config["UPLOADED_PHOTOS_DEST"] = os.path.join(basedir, 'static/img')
+
+# Photo Upload Setup Sourced from FLASK_UPLOADS Documentaion page https://pythonhosted.org/Flask-Uploads/
+photos = UploadSet('photos', IMAGES)  # Flask-Uploads Upload Sets
+configure_uploads(app, photos)
+
 
 db = SQLAlchemy(app)
 
