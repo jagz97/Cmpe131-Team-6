90d6a0b32af19525a76bb8053e3763f9c562d509
jagjitsingh <jagjit.saini2019@gmail.com>
2022-04-30 23:03:28 -0700

    add products, categories, brands and add pictures of items including display items with pictures on home page completed

--- app/models.py
+++ app/models.py
@@ -1,29 +1,44 @@
+from sqlalchemy import Column, ForeignKey, false
 from app import db
+from datetime import datetime
+
+# Source Code: Simple Relations https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/
+
+
+class AddProduct(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    discount = db.Column(db.Integer, default=0)
+    # Integer with 2 decimal places
+    price = db.Column(db.Numeric(10, 2), nullable=False)
+    availablestock = db.Column(db.Integer, nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    category_id = db.Column(db.Integer, db.ForeignKey(
+        'category.id'), nullable=False)
+    category = db.relationship(
+        'Category', backref=db.backref('categories', lazy=True))
+
+    brand_id = db.Column(db.Integer, db.ForeignKey('brand.id'), nullable=False)
+    brand = db.relationship('Brand', backref=db.backref('brands', lazy=True))
+
+    image = db.Column(db.String(150), nullable=False)
+    image_1 = db.Column(db.String(150), nullable=False)
+    image_2 = db.Column(db.String(150), nullable=False)
+
+    def __repr__(self):
+        return '<AddProduct %r>' % self.name
+
 
 class Brand (db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(30), nullable=False, unique =True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
+
 
 class Category(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(30), nullable=False, unique =True)
-
-class Address(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    full_name = db.Column(db.String(128), nullable=False)
-    address_line_one = db.Column(db.String(256), nullable=False)
-    address_line_two = db.Column(db.Integer)
-    city = db.Column(db.String(128), nullable=False)
-    state_province_region = db.Column(db.String(128), nullable=False)
-    zip_postal_code = db.Column(db.String(32), nullable=False)
-    country = db.Column(db.String(64), nullable=False)
-    user = db.relationship('User', back_populates='Address')
-
-class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), index=True, nullable=False, unique=True)
-    email = db.Column(db.String(30), index =True, nullable=False, unique=True)
-    password_hash = db.Column(db.String(128)
-    shipping_address = db.relationship('Address', back_populates='User', uselist=False)
+    name = db.Column(db.String(30), nullable=False, unique=True)
+
 
 db.create_all()
