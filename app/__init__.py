8534bc783f6a4b6bbd3de41d4a60d758c817fb6b
jagz97 <68725607+jagz97@users.noreply.github.com>
2022-05-09 23:03:19 -0700

    buy items with stripe fake card entry

--- app/__init__.py
+++ app/__init__.py
@@ -17,17 +17,16 @@ app.config.from_mapping(
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 )
 
-# Flask-Uploads configration provided on Documentation Page
+# Flask-Uploads configuration provided on Documentation Page
 app.config["UPLOADED_PHOTOS_DEST"] = os.path.join(basedir, 'static/img')
 
-# Photo Upload Setup Sourced from FLASK_UPLOADS Documentaion page https://pythonhosted.org/Flask-Uploads/
+# Photo Upload Setup Sourced from FLASK_UPLOADS Documentation page https://pythonhosted.org/Flask-Uploads/
 photos = UploadSet('photos', IMAGES)  # Flask-Uploads Upload Sets
 configure_uploads(app, photos)
 
-
 db = SQLAlchemy(app)
 search = Search()
-search.init_app(app) # initalize search
+search.init_app(app)  # initialize search
 
 login = LoginManager(app)
 login.login_view = 'login'
